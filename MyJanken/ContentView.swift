007e82c7bee4cee6aaae50e39ce0cebbb3cd72ed
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-11-24 10:20:20 +0900

    ✨ ランダムでじゃんけん画像を出力

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -1,9 +1,29 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State var ansNum = -1
+  let imgName = ["gu", "choki", "pa"]
+  let imgStr = ["グー", "チョキ", "パー"]
+  
   var body: some View {
     VStack {
-      Image("gu")
+      if ansNum == -1 {
+        Text("これからじゃんけんをします")
+      } else {
+        Image(imgName[ansNum])
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+        Text(imgStr[ansNum])
+      }
+      Button("じゃんけんをするぞ！"){
+        print("タップされたぞ")
+        ansNum = Int.random(in: 0...2)
+      }
+        .frame(maxWidth: .infinity)
+        .frame(height: 100)
+        .font(.title)
+        .background(Color.pink)
+        .foregroundColor(Color.white)
     }
     .padding()
   }
