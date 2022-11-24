38bc63935f8bd296a192b1e5648e701c1bb7f35f
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-11-24 09:17:43 +0900

    🌱 First commit

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  MyJanken
-//
-//  Created by 磯野圭希 on 2022/11/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
--- MyJanken/MyJankenApp.swift
+++ MyJanken/MyJankenApp.swift
@@ -1,10 +1,3 @@
-//
-//  MyJankenApp.swift
-//  MyJanken
-//
-//  Created by 磯野圭希 on 2022/11/24.
-//
-
 import SwiftUI
 
 @main
