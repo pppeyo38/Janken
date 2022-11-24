d57b5bb8827ed284bd1da1a9124dadb1290ad25f
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-11-24 09:16:34 +0900

    Initial Commit

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by 磯野圭希 on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
