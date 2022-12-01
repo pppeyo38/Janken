2d47646cf520473e11ed5b34badd98ea3f6606df
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-12-01 09:41:16 +0900

    ✨ 使用変数の追加・横並びのじゃんけんボタン作成

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -1,35 +1,47 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State var ansNum = -1
-  let imgName = ["gu", "choki", "pa"]
-  let imgStr = ["グー", "チョキ", "パー"]
+  @State var ansNum = -1 // コンピューターの手
+  @State var selNum = -1 // 自分の手
+  @State var resultNum = -1 // 結果
+  @State var wld = [0, 0, 0] // 通算の勝ち・負け・引き分け
+  @State var resultText = "" // 戦績の文字列
+  
+  let imgName = ["gu", "choki", "pa"] // 画像名
+  let imgStr = ["グー", "チョキ", "パー"] // 自分の手の表示用
+  let resultStr = ["勝ち", "負け", "引き分け"] // 結果の表示用
+  let resultUnit = ["勝", "敗", "分"] // 戦績の表示用
+  let tabWL = [[2, 0, 1], [1, 2, 0], [0, 1, 2]] // 対戦表
   
   var body: some View {
     VStack {
-      Spacer()
-      if ansNum == -1 {
-        Text("これからじゃんけんをします")
-          .padding(.bottom)
-      } else {
-        Image(imgName[ansNum])
-          .resizable()
-          .aspectRatio(contentMode: .fit)
-        Spacer()
-        Text(imgStr[ansNum])
-          .padding(.bottom)
+      HStack {
+        Button("グー"){
+          
+        }
+          .frame(maxWidth: .infinity)
+          .frame(height: 100)
+          .font(.title)
+          .background(Color.pink)
+          .foregroundColor(Color.white)
+        Button("チョキ"){
+          
+        }
+          .frame(maxWidth: .infinity)
+          .frame(height: 100)
+          .font(.title)
+          .background(Color.pink)
+          .foregroundColor(Color.white)
+        Button("パー"){
+          
+        }
+          .frame(maxWidth: .infinity)
+          .frame(height: 100)
+          .font(.title)
+          .background(Color.pink)
+          .foregroundColor(Color.white)
       }
-      Button("じゃんけんをするぞ！"){
-        print("タップされたぞ")
-        ansNum = Int.random(in: 0...2)
-      }
-        .frame(maxWidth: .infinity)
-        .frame(height: 100)
-        .font(.title)
-        .background(Color.pink)
-        .foregroundColor(Color.white)
     }
-    .padding()
   }
 }
 
