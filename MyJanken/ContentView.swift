9d48a12655542f499b744fb396d566d03f34394a
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-12-01 09:59:40 +0900

    🎨 Layout

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -9,39 +9,75 @@ struct ContentView: View {
   
   let imgName = ["gu", "choki", "pa"] // 画像名
   let imgStr = ["グー", "チョキ", "パー"] // 自分の手の表示用
-  let resultStr = ["勝ち", "負け", "引き分け"] // 結果の表示用
+  let resultStr = ["勝ち！", "負け…", "引き分け"] // 結果の表示用
   let resultUnit = ["勝", "敗", "分"] // 戦績の表示用
   let tabWL = [[2, 0, 1], [1, 2, 0], [0, 1, 2]] // 対戦表
   
   var body: some View {
     VStack {
+      // 戦績
+      Text(resultText)
+        .font(.title)
+      
+      // 画像と結果
+      if ansNum == -1 {
+        // 初期状態
+        Text("これからじゃんけんをします")
+          .padding(.bottom)
+      } else {
+        // 画像
+        Image(imgName[ansNum])
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+        Image(imgName[selNum])
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+        // 画像
+        Text(resultStr[resultNum])
+          .padding(.bottom)
+      }
+      
+      // じゃんけん結果
+      Text(resultStr[0])
+        .font(.title)
+      
+      // 自分の出す手を決めるボタン
       HStack {
-        // 自分の出す手を決めるボタン
-        Button("グー"){
-          
+        Button(imgStr[0]){
+          selNum = 0
         }
           .frame(maxWidth: .infinity)
-          .frame(height: 70)
+          .frame(height: 80)
           .font(.title2)
           .background(Color.purple)
           .foregroundColor(Color.white)
-        Button("チョキ"){
-          
+        Button(imgStr[1]){
+          selNum = 1
         }
           .frame(maxWidth: .infinity)
-          .frame(height: 70)
+          .frame(height: 80)
           .font(.title2)
           .background(Color.purple)
           .foregroundColor(Color.white)
-        Button("パー"){
-          
+        Button(imgStr[2]){
+          selNum = 1
         }
           .frame(maxWidth: .infinity)
-          .frame(height: 70)
+          .frame(height: 80)
           .font(.title2)
           .background(Color.purple)
           .foregroundColor(Color.white)
       }
+      
+      // リセットボタン
+      Button("リセット"){
+        
+      }
+        .frame(maxWidth: .infinity)
+        .frame(height: 60)
+        .font(.title2)
+        .background(Color.blue)
+        .foregroundColor(Color.white)
     }
     .padding()
   }
